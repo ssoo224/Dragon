226a0ae8c1700f5f31a82df5978889a645a9b90c
علوش <anldhe.b42.9@gmail.com>
2024-12-07 11:38:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const dragon = document.getEl
+
+ementById('dragon');
+
+document.addEventListener
+
+('mousemove', (event) =>
+
+{
+
+const x = event.clientX;
+
+const y = event.clientY;
+
+dragon.style.left = ${x}
+
+px;
+
+dragon.style.top = `${y}
+
+px;
+
+;({
